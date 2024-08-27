679a45e0a1371b54d1632022f5d1adf64979c44e
Tushar Parmar <parmart364@gmail.com>
2024-08-27 06:54:29 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+" use strict"; // treat all js code as new version 
+ 
+// alert(3+3) //  we are using nodejs , not browser
+
+console.log(3
+    +3)   // code readability should be high 
+
+    console.log("Tushar")
+
+    // Boolean type 
+    let name = "tushar"
+    let age = 20 
+    let  isloggeIn = false
+    let state;
+
+    // data type  
+
+    // Number => 2 to power 54 
+    // BigInt 
+    // String => ""
+    // boolean => true / false 
+    // null => is standalone value 
+    // undefined =>
+    //symbol  => unique 
+    
+    
+    //object 
+
+    console.log (typeof "tushar"); // its is string 
+    console.log (typeof age ); // its is number
+    console.log (typeof null); // its is null 
+    console.log (typeof undefined);  // its is undefined 
+
+
