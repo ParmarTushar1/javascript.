cb5008fcf04bfeb9666217659ca7e9876ff94133
Tushar Parmar <parmart364@gmail.com>
2024-09-03 12:20:28 +0000

    conversion is done

---
+++ 01_basics/conversionOpertion.js
@@ -0,0 +1,35 @@
+// let score =33                //number
+// let score = "ABC"               //string  
+// let score ="33ABC"           //string
+// let score = null                 //pnject
+// let score = undefined              //undefined
+// let score = true                  //1
+// let score = false                  //0
+// let score = "tushar"                //NaN
+ 
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score) 
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); 
+
+
+let isloggeIn = "tushar"   //true  
+         // "      ""      // false  
+         // "       1      //true
+         // "       0      //false
+                 
+
+let booleanIsLoggedIn = Boolean(isloggeIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber =33
+let stringNumber =String(someNumber)
+
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
+//output is 
+//33
+//String
