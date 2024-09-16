c3f36b42b4041b1956fba3989a06c7ea6ab98d09
Tushar Parmar <parmart364@gmail.com>
2024-09-16 10:39:17 +0000

    commit don variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -14,25 +14,39 @@ accuntcity = "mumbai"
 console.log(accuntId);
 console.table([accuntId,accuntEmail,accuntpassword,accuntcity,accuntstate])
 
-//how to run ls/node and tab 
+//how to run :- ls/node and tab 
+
+//in javaScript  Var,let and const are used for varible declaration ,
+//but thay have different behaviors and use cases:
+
+//const
+// . block-scoped 
+// . hoisted but not initialized 
+// . connot be reassigned after its initial value (immutable)
+
+//let
+// . blcke-scoped (works within a block {})
+// . hoisted but not initialized ; accessing before declaration give ReferenceError.
+// . cannot be redeclared in the same scope.
+
+//var
+// . function-scoped (works within the function).
+// . hoisted and initialized with Undefined.
+// . can be redeclared in the same scope. 
+
+
 
 // prefer not to use var 
 // because of issue in block scope and functionl scope
  
-// const 
-// Variables declared with const are also block-scoped like let.
-// Variables declared with const cannot be reassigned once they are initialized. However,
-// if the value is an object or an array, the properties or elements of the object or array can 
-// still be mutated.
+//summary 
+ // vae :- old way , function-scoped.
+ // let :- new way , block-scoped, can be changed.
+ // const :- new way , block-scoped, connot be changed.
 
-// lat
-// Variables declared with let are block-scoped, meaning their scope is limited to the 
-// block in which they are defined (e.g., within a loop or conditional statement).
-// Variables declared with let cannot be redeclared within the same block, but they can 
-// be reassigned.
 
-// summary 
-// Use let for variables that need to be block-scoped and may require reassignment but not 
-// redeclaration.
-// Use const for variables that need to be block-scoped and should not be reassigned after 
-// initialization, although their properties or elements can still be mutated.
+
+
+
+
+
