8267e18d57960b789249734a5a4cc49bc860e773
Tushar Parmar <parmart364@gmail.com>
2024-09-26 13:36:55 +0000

    commit  don

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -33,7 +33,7 @@ console.table([accuntId,accuntEmail,accuntpassword,accuntcity,accuntstate])
 // . function-scoped (works within the function).
 // . hoisted and initialized with Undefined.
 // . can be redeclared in the same scope. 
-
+                   
 // prefer not to use var 
 // because of issue in block scope and functionl scope
  
---
+++ 01_basics/map_filter_reduce.js
@@ -0,0 +1,19 @@
+// in react , the map , filter , and reduce 
+// Array methods are frequently used for handling
+// and transforming data in a concise and functional manner.
+
+// Map()
+// the Map function is used to create a new Array
+//by appliying a function to each Element of an existing  Array .it donesn't 
+//modify the original Array
+
+
+// filter ()
+//the filter function creates a new Array containing 
+//only Elements that pass a certain test implemented by the provided function.
+//it donesn't modify the original Array.
+
+reduce ()
+//the reduce function executes a reducer function on each Element of the Array
+//resulting in a Signal output value.  it doesn't modify the original Array
+ 
