333fa33b1a080a4537090af6e9e1ad592a5945b9
Tushar Parmar <parmart364@gmail.com>
2024-10-03 07:59:50 +0000

    datatype is don

---
+++ 01_basics/datatypesummary.js
@@ -0,0 +1,46 @@
+// tow type of datatype in javaScript primitive and non-primitive
+
+//primitive datatype 
+// types 7 :- string , number , Boollearn , null , symbol , Bigint
+
+//String: Represents textual data. Example: 'Hello', "World".
+//Number: Represents both integer and floating-point numbers. Example: 42, 3.14.
+//Boolean: Represents logical values, either true or false. Example: true, false.
+//Undefined: A variable that has been declared but not assigned a value. 
+//Example: let x; console.log(x); // undefined.
+//Null: Represents the intentional absence of any object value.
+// Example: let y = null;.
+//Symbol (ES6): Represents a unique and immutable identifier, often used for object properties.
+// Example: let sym = Symbol('desc');.
+//BigInt (ES2020): Used for representing integers larger than 2^53 - 1, which is the limit for the Number type. 
+//Example: let bigInt = 123456789012345678901234567890n;.
+
+//non-primitive
+
+
+//Array: An ordered list of values (indexed collection).
+// Example: let arr = [1, 2, 3];.
+//Function: A block of code that can be invoked or called.
+// Example: function greet() { console.log('Hello'); }.
+//Date: Used for handling dates and times.
+// Example: let now = new Date();.
+//RegExp: Represents regular expressions for pattern matching.
+// Example: let regex = /abc/;.
+
+
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
